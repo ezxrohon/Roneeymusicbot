12cde881627872f3968b22554d8cf4c6c1fb93c0
Artist <satyamshiv55@gmail.com>
2026-06-16 00:09:04 +0530

    ✨𝗔𝗿𝘁𝗶𝘀𝘁𝗯𝗼𝘁𝘀 ✨

--- Elevenyts/helpers/__init__.py
+++ Elevenyts/helpers/__init__.py
@@ -1,26 +1,17 @@
 # ==========================================================
-# Copyright (c) 2026 ArtistBots
-# All Rights Reserved. 
-# 
-# Project      : ArtistBots API Telegram Music Bot
-# Powered By   : Artist
-# Type         : API Based Telegram Music Bot
-#
-# Bot          : @ArtistApibot
-# Channel      : https://t.me/artistbots
-# GitHub       : https://github.com/elevenyts
-#
-# Unauthorized copying, modification, or redistribution
-# of this source code without permission is prohibited.
-# ==========================================================
-from ._admins import admin_check, can_manage_vc, is_admin, reload_admins, can_manage_vc_channel
-from ._dataclass import Media, Track
-from ._exec import format_exception, meval
-from ._inline import Inline
-from ._queue import Queue
-from ._thumbnails import Thumbnail
-from ._utilities import Utilities
-
-buttons = Inline()
-thumb = Thumbnail()
-utils = Utilities()
+  # Copyright (c) 2026 ArtistBots
+  # All Rights Reserved.
+  #
+  # Project      : ArtistBots API Telegram Music Bot
+  # Powered By   : Artist
+  # Type         : API Based Telegram Music Bot
+  #
+  # Bot          : @ArtistApibot
+  # Channel      : https://t.me/artistbots
+  # GitHub       : https://github.com/elevenyts
+  #
+  # Unauthorized copying, modification, or redistribution
+  # of this source code without permission is prohibited.
+  # ==========================================================
+import base64
+exec(base64.b64decode("IyA9PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09CiMgQ29weXJpZ2h0IChjKSAyMDI2IEFydGlzdEJvdHMKIyBBbGwgUmlnaHRzIFJlc2VydmVkLgojCiMgUHJvamVjdCAgICAgIDogQXJ0aXN0Qm90cyBBUEkgVGVsZWdyYW0gTXVzaWMgQm90CiMgUG93ZXJlZCBCeSAgIDogQXJ0aXN0CiMgVHlwZSAgICAgICAgIDogQVBJIEJhc2VkIFRlbGVncmFtIE11c2ljIEJvdAojCiMgQm90ICAgICAgICAgIDogQEFydGlzdEFwaWJvdAojIENoYW5uZWwgICAgICA6IGh0dHBzOi8vdC5tZS9hcnRpc3Rib3RzCiMgR2l0SHViICAgICAgIDogaHR0cHM6Ly9naXRodWIuY29tL2VsZXZlbnl0cwojCiMgVW5hdXRob3JpemVkIGNvcHlpbmcsIG1vZGlmaWNhdGlvbiwgb3IgcmVkaXN0cmlidXRpb24KIyBvZiB0aGlzIHNvdXJjZSBjb2RlIHdpdGhvdXQgcGVybWlzc2lvbiBpcyBwcm9oaWJpdGVkLgojID09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT0KZnJvbSAuX2FkbWlucyBpbXBvcnQgYWRtaW5fY2hlY2ssIGNhbl9tYW5hZ2VfdmMsIGlzX2FkbWluLCByZWxvYWRfYWRtaW5zLCBjYW5fbWFuYWdlX3ZjX2NoYW5uZWwKZnJvbSAuX2RhdGFjbGFzcyBpbXBvcnQgTWVkaWEsIFRyYWNrCmZyb20gLl9leGVjIGltcG9ydCBmb3JtYXRfZXhjZXB0aW9uLCBtZXZhbApmcm9tIC5faW5saW5lIGltcG9ydCBJbmxpbmUKZnJvbSAuX3F1ZXVlIGltcG9ydCBRdWV1ZQpmcm9tIC5fdGh1bWJuYWlscyBpbXBvcnQgVGh1bWJuYWlsCmZyb20gLl91dGlsaXRpZXMgaW1wb3J0IFV0aWxpdGllcwoKYnV0dG9ucyA9IElubGluZSgpCnRodW1iID0gVGh1bWJuYWlsKCkKdXRpbHMgPSBVdGlsaXRpZXMoKQo=").decode("utf-8"))
