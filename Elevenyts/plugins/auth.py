4a2a373c7676ed0f87a49948e3557c582b0e8d6e
ezxrohon <rohonkazi004@gmail.com>
2026-06-16 10:19:33 +0530

    Update auth.py

--- Elevenyts/plugins/auth.py
+++ Elevenyts/plugins/auth.py
@@ -3,12 +3,12 @@
   # All Rights Reserved.
   #
   # Project      : ArtistBots API Telegram Music Bot
-  # Powered By   : Artist
+  # Powered By   : Ꮢᴏʜꫝɴ ⁣
   # Type         : API Based Telegram Music Bot
   #
   # Bot          : @ArtistApibot
   # Channel      : https://t.me/artistbots
-  # GitHub       : https://github.com/elevenyts
+  # GitHub       : https://github.com/ezxrohon/Roneeymusicbot
   #
   # Unauthorized copying, modification, or redistribution
   # of this source code without permission is prohibited.
